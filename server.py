6f33dcc8533d5bbe895fd9fd9bea77b988f0675a
on3gin <t22092006@gmail.com>
2023-04-01 07:42:25 +0300

    full-files 3

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
+import os
 
 
 app = Flask(__name__)
+os.chdir('static//img')
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def form():
-    return render_template('index.html')
+    if request.method == 'GET':
+        files = os.listdir(path=".")
+        return render_template('index.html', title='Карусель', files=files[1:])
+    elif request.method == 'POST':
+        img = request.files['file']
+        img.save(img.filename)
+        return 'Файл успешно загружен, обновите страницу'
 
 
 if __name__ == '__main__':
